6245dfebb78e7ff0aba94079012262d9c7f0d777
Tony Muir <tmuir12@gmail.com>
2014-11-08 12:46:23 +0800

    Changed from a windows to linux config file

--- config.sample.php
+++ config.sample.php
@@ -6,22 +6,22 @@
 
 // Communicate with bitcoind via JSON RPC calls:
 
-define('USERNAME', 	'test');
+define('USERNAME', 	'rpcuser');
 define('PASSWORD', 	'test');
 define('SCHEME',	'http');        // http  or https  
 define('HOST',     	'127.0.0.1');   
-define('PORT',     	'8332');       
+define('PORT',     	'9332');       
 
-define('SERVER_NETWORK', 'Bitcoin');     // Display name of the network
+define('SERVER_NETWORK', 'Feathercoin');     // Display name of the network
 
 // Windows Localhost Server
 
 define('SERVER_LOCALHOST', 		true);   // is server on localhost? true / false
-define('SERVER_LOCALHOST_TYPE', 'windows');  // type:  windows, linux
-define('WINDOWS_TASKLIST', 		'C:\Windows\System32\tasklist.exe');
+define('SERVER_LOCALHOST_TYPE', 'linux');  // type:  windows, linux
+#define('WINDOWS_TASKLIST', 		'C:\Windows\System32\tasklist.exe');
 
-define('SERVER',         		'C:\tmp\bitcoin\bitcoind.exe');  // full pathname to bitcoind executable
-define('SERVER_NAME',    		'bitcoind.exe'); // name only of bitcoind executable
+define('SERVER',         		'/usr/local/bin/featehrcoind');  // full pathname to bitcoind executable
+define('SERVER_NAME',    		'featehrcoind'); // name only of bitcoind executable
 define('SERVER_TESTNET',    	true); // use testnet?  true / false
-define('SERVER_DATADIR', 		'C:\tmp\bitcoin-testnet-datadir');  // location of data dir
-define('SERVER_CONF',    		'C:\tmp\bitcoin-testnet-datadir\test.conf'); // location of conf file
+define('SERVER_DATADIR', 		'/home/pi/.feathercoin');  // location of data dir
+define('SERVER_CONF',    		'/home/pi/featehrcoin/feathercoin.conf'); // location of conf file
